2e09eeafcb2eba503f73dbf464246745a273e1d1
Eduardo Lacerda <lacerda.snr@gmail.com>
2018-07-23 21:59:22 -0300

    Mongodb Setup - ok

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import productsRouter from './server/routes/products';
 
+import db from './server/db/db';
+
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -10,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+// start database
+db.start();
 
 // routes
 app.use('/api', productsRouter);
