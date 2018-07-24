7e97ddccb0fccc45b1de255389645d16b519759e
Eduardo Lacerda <lacerda.snr@gmail.com>
2018-07-23 21:28:20 -0300

    Express setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import path from 'path';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-console.log(__dirname);
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Treinamento web... primeira rota!');
+});
+
+app.listen(port, () => {
+    console.info(`App is running on port ${port}`);
+});
